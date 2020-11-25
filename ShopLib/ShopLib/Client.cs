37224a733c26bd57f72e5c1664eab5696d47238e
Małgosia Sobocińska <sobocinska.malgo@gmail.com>
2020-11-25 11:30:36 +0100

    added new project ShopLib

---
+++ ShopLib/ShopLib/Client.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShopLib
+{
+    public class Client
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Name => FirstName + " " + LastName;
+        public string Address { get; set; }
+
+        private List<Item> items = new List<Item>();
+
+        public List<Item> Items
+        {
+            get
+            {
+                // Copy of original "items" collection
+                return new List<Item>(items);
+            }
+        }
+
+        internal void AddItem(Item item)
+        {
+            if (item == null) throw new ArgumentNullException(nameof(item));
+            items.Add(item);
+        }
+
+        internal void RemoveItem(Item item)
+        {
+            items.Remove(item);
+        }
+
+        internal void RemoveItemById(int id)
+        {
+            foreach (Item tmp in items)
+            {
+                if (tmp.ItemId == id)
+                {
+                    items.Remove(tmp);
+                    return;
+                }
+            }
+        }
+
+        internal Client(string firstName, string lastName, string address)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Address = address;
+        }
+    }
+}
---
+++ ShopLib/ShopLib/Item.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShopLib
+{
+    public class Item
+    {
+        static int nextId;
+        public int ItemId { get; private set; }
+
+        public string ProductName { get; set; }
+        public decimal UnitPrice { get; set; }
+        public uint Amount { get; set; }
+
+        public decimal Subtotal => UnitPrice * Amount;
+
+        public override string ToString()
+        {
+            return string.Format("{0}. {1}, amount: {2}, price: {3}",
+                ItemId, ProductName, Amount, Subtotal);
+        }
+
+        public Item()
+        {
+            ItemId = GenerateId();
+        }
+
+        public Item(string name, decimal price)
+        {
+            ItemId = GenerateId();
+            ProductName = name;
+            UnitPrice = price;
+            Amount = 1;
+        }
+
+        public Item(string name, decimal price, uint amount)
+        {
+            ItemId = GenerateId();
+            ProductName = name;
+            UnitPrice = price;
+            Amount = amount;
+        }
+
+        protected static int GenerateId()
+        {
+            // http://stackoverflow.com/a/9262259/1442833
+            return Interlocked.Increment(ref nextId);
+        }
+    }
+}
