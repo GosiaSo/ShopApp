fb6364029a643a7b6dac45fcf35760bc8c132ad0
Małgosia Sobocińska <sobocinska.malgo@gmail.com>
2020-12-05 14:14:29 +0100

    added listbox data template added methods to events onclick

--- ShopApp/MainWindow.xaml.cs
+++ ShopApp/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ShopLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,51 @@ namespace ShopApp
     /// </summary>
     public partial class MainWindow : Window
     {
+        private Shop shop = new Shop();
+
         public MainWindow()
         {
             InitializeComponent();
+            
+        }
+
+        private void OdswiezKlientow()
+        {
+            listBoxKlienci.Items.Clear();
+            foreach (Client client in shop.Clients)
+            {
+                listBoxKlienci.Items.Add(client);
+            }
+        }
+
+        private void buttonOdswiez_Click(object sender, RoutedEventArgs e)
+        {
+            OdswiezKlientow();
+        }
+
+        private void buttonRejestruj_Click(object sender, RoutedEventArgs e)
+        {
+            string firstName = textBoxImie.Text;
+            string lastName = textBoXNazwisko.Text;
+            string address = textBoXAdres.Text;
+            shop.RegisterClient(firstName, lastName, address);
+            OdswiezKlientow();
+        }
+
+        private void buttonUsuńKlienta_Click(object sender, RoutedEventArgs e)
+        {
+            //object obj = listBoxKlienci.SelectedItem;
+            //Client client = (Client)obj;   
+            // but what if I have not any client in these objects?
+
+
+            if (listBoxKlienci.SelectedItem is Client client)
+            {
+                shop.RemoveClient(client);
+                OdswiezKlientow();
+            }
+            else
+                MessageBox.Show("Nie można wykonać operacji", "Błąd", MessageBoxButton.OK, MessageBoxImage.Error);
         }
     }
 }
